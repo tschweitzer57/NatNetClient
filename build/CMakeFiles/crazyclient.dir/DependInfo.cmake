275515730b204e6032770075a33fd62bc66c6f70
tschweitzer57 <thibault.schweitzer@estaca.eu>
2025-11-20 13:44:50 +0100

    first commit

---
+++ build/CMakeFiles/crazyclient.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/estacalab/Documents/MOCAP/3_NatNet_customcpp/src/crazyclient.cpp" "CMakeFiles/crazyclient.dir/src/crazyclient.cpp.o" "gcc" "CMakeFiles/crazyclient.dir/src/crazyclient.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/crazyclient.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for crazyclient.
+# This may be replaced when dependencies are built.
